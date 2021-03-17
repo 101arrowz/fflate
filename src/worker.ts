c5405aabf7f7a9597bac7ea6a8a361b6cc635bca
Arjun Barrett <arjunbarrett@gmail.com>
2021-03-16 17:27:19 -0700

    Add Deno support

--- src/worker.ts
+++ src/worker.ts
@@ -1,7 +1,15 @@
 const ch2: Record<string, string> = {};
 
+let durl = (c: string) => URL.createObjectURL(new Blob([c], { type: 'text/javascript' }));
+
+try {
+  durl('')
+} catch(e) {
+  durl = c => 'data:application/javascript;charset=UTF-8,' + encodeURI(c);
+}
+
 export default <T>(c: string, id: number, msg: unknown, transfer: ArrayBuffer[], cb: (err: Error, msg: T) => void) => {
-  const u = ch2[id] ||= URL.createObjectURL(new Blob([c], { type: 'text/javascript' }));
+  const u = ch2[id] ||= durl(c);
   const w = new Worker(u);
   w.onerror = e => cb(e.error, null);
   w.onmessage = e => cb(null, e.data);
