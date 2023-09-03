3ca080a3a757a81d5cc24e71b5bb2dc8e51f9c0d
101arrowz <arjunbarrett@gmail.com>
2023-09-03 10:33:06 -0400

    Retain .js extension

--- scripts/rewriteBuilds.ts
+++ scripts/rewriteBuilds.ts
@@ -1,7 +1,7 @@
-import { readFileSync, writeFileSync, unlinkSync, renameSync } from 'fs';
+import { readFileSync, writeFileSync, unlinkSync } from 'fs';
 import { join } from 'path';
 const atClass = /\/\*\* \@class \*\//g, pure = '/*#__PURE__*/';
-const esModule = /exports.__esModule = true;\n/;
+const esModule = /exports\.__esModule = true;\n/;
 const libDir = join(__dirname, '..', 'lib');
 const libIndex = join(libDir, 'index.js');
 const lib = readFileSync(libIndex, 'utf-8')
@@ -24,13 +24,13 @@ writeFileSync(join(libDir, 'worker.cjs'), readFileSync(join(libDir, 'worker.js')
 writeFileSync(join(libDir, 'node-worker.cjs'), readFileSync(join(libDir, 'node-worker.js'), 'utf-8').replace(esModule, ''));
 unlinkSync(esmIndex), unlinkSync(esmWK), unlinkSync(esmNWK), unlinkSync(libIndex), unlinkSync(libWK), unlinkSync(libNWK);
 unlinkSync(join(libDir, 'index.d.ts')), unlinkSync(join(libDir, 'worker.d.ts')), unlinkSync(join(libDir, 'node-worker.d.ts'));
-const workerImport = /import (.*) from '\.\/node-worker';/;
-const workerRequire = /var (.*) = require\("\.\/node-worker"\);/;
+const workerImport = /import (.*?) from '\.\/node-worker';/;
+const workerRequire = /var (.*?) = require\("\.\/node-worker"\);/;
 const defaultExport = /export default/;
 writeFileSync(join(esmDir, 'index.mjs'), "import { createRequire } from 'module';\nvar require = createRequire('/');\n" + esm.replace(workerImport, name => nwk.replace(defaultExport, `var ${name.slice(7, name.indexOf(' ', 8))} =`)));
 writeFileSync(join(esmDir, 'index.d.mts'), libTypes);
-writeFileSync(join(esmDir, 'browser.mjs'), esm.replace(workerImport, name => wk.replace(defaultExport, `var ${name.slice(7, name.indexOf(' ', 8))} =`)));
-writeFileSync(join(esmDir, 'browser.d.mts'), libTypes);
+writeFileSync(join(esmDir, 'browser.js'), esm.replace(workerImport, name => wk.replace(defaultExport, `var ${name.slice(7, name.indexOf(' ', 8))} =`)));
+writeFileSync(join(esmDir, 'browser.d.ts'), libTypes);
 writeFileSync(join(libDir, 'node.cjs'), lib.replace(workerRequire, name => {
   name = name.slice(4, name.indexOf(' ', 5));
   return nwk.replace(defaultExport, `var ${name} = {};\n${name}["default"] =`)
@@ -42,4 +42,4 @@ writeFileSync(join(libDir, 'browser.cjs'), lib.replace(workerRequire, name => {
 }));
 writeFileSync(join(libDir, 'browser.d.cts'), libTypes);
 writeFileSync(join(libDir, 'index.cjs'), lib.replace(workerRequire, name => `var ${name.slice(4, name.indexOf(' ', 5))} = require("./node-worker.cjs");`));
-writeFileSync(join(libDir, 'index.d.cts'), libTypes);
+writeFileSync(join(libDir, 'index.d.ts'), libTypes);
