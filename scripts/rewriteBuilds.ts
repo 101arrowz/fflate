c36d658843e3bb0967d82e2abe249831a40e39f5
Nathan Bierema <nbierema@gmail.com>
2023-09-03 10:34:52 -0400

    Fix types ESM module resolution (#170)
    * Fix types ESM module resolution
    
    * Retain .js extension
    
    ---------
    
    Co-authored-by: 101arrowz <arjunbarrett@gmail.com>

--- scripts/rewriteBuilds.ts
+++ scripts/rewriteBuilds.ts
@@ -1,13 +1,14 @@
-import { readFileSync, writeFileSync, unlinkSync, renameSync } from 'fs';
+import { readFileSync, writeFileSync, unlinkSync } from 'fs';
 import { join } from 'path';
 const atClass = /\/\*\* \@class \*\//g, pure = '/*#__PURE__*/';
-const esModule = /exports.__esModule = true;\n/;
+const esModule = /exports\.__esModule = true;\n/;
 const libDir = join(__dirname, '..', 'lib');
 const libIndex = join(libDir, 'index.js');
 const lib = readFileSync(libIndex, 'utf-8')
   .replace(atClass, pure)
   .replace(esModule, '')
   .replace(/exports\.(.*) = void 0;\n/, '');
+const libTypes = readFileSync(join(libDir, 'index.d.ts'), 'utf-8');
 
 writeFileSync(libIndex, lib);
 const esmDir = join(__dirname, '..', 'esm');
@@ -22,18 +23,23 @@ const wk = readFileSync(esmWK, 'utf-8'),
 writeFileSync(join(libDir, 'worker.cjs'), readFileSync(join(libDir, 'worker.js'), 'utf-8').replace(esModule, ''));
 writeFileSync(join(libDir, 'node-worker.cjs'), readFileSync(join(libDir, 'node-worker.js'), 'utf-8').replace(esModule, ''));
 unlinkSync(esmIndex), unlinkSync(esmWK), unlinkSync(esmNWK), unlinkSync(libIndex), unlinkSync(libWK), unlinkSync(libNWK);
-unlinkSync(join(libDir, 'worker.d.ts')), unlinkSync(join(libDir, 'node-worker.d.ts'));
-const workerImport = /import (.*) from '\.\/node-worker';/;
-const workerRequire = /var (.*) = require\("\.\/node-worker"\);/;
+unlinkSync(join(libDir, 'index.d.ts')), unlinkSync(join(libDir, 'worker.d.ts')), unlinkSync(join(libDir, 'node-worker.d.ts'));
+const workerImport = /import (.*?) from '\.\/node-worker';/;
+const workerRequire = /var (.*?) = require\("\.\/node-worker"\);/;
 const defaultExport = /export default/;
 writeFileSync(join(esmDir, 'index.mjs'), "import { createRequire } from 'module';\nvar require = createRequire('/');\n" + esm.replace(workerImport, name => nwk.replace(defaultExport, `var ${name.slice(7, name.indexOf(' ', 8))} =`)));
+writeFileSync(join(esmDir, 'index.d.mts'), libTypes);
 writeFileSync(join(esmDir, 'browser.js'), esm.replace(workerImport, name => wk.replace(defaultExport, `var ${name.slice(7, name.indexOf(' ', 8))} =`)));
+writeFileSync(join(esmDir, 'browser.d.ts'), libTypes);
 writeFileSync(join(libDir, 'node.cjs'), lib.replace(workerRequire, name => {
   name = name.slice(4, name.indexOf(' ', 5));
   return nwk.replace(defaultExport, `var ${name} = {};\n${name}["default"] =`)
 }));
+writeFileSync(join(libDir, 'node.d.cts'), libTypes);
 writeFileSync(join(libDir, 'browser.cjs'), lib.replace(workerRequire, name => {
   name = name.slice(4, name.indexOf(' ', 5));
   return wk.replace(defaultExport, `var ${name} = {};\n${name}["default"] =`)
 }));
+writeFileSync(join(libDir, 'browser.d.cts'), libTypes);
 writeFileSync(join(libDir, 'index.cjs'), lib.replace(workerRequire, name => `var ${name.slice(4, name.indexOf(' ', 5))} = require("./node-worker.cjs");`));
+writeFileSync(join(libDir, 'index.d.ts'), libTypes);
